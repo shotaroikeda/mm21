54ef29093e1374eff22df9c414605df1cf46a433
Eric Sauer <ericmsauer@gmail.com>
2015-02-21 16:48:28 -0800

    init set up for server code

---
+++ src/server/server_contants.py
@@ -0,0 +1,12 @@
+"serverDefaults": {
+"players": 2,
+"map": "rooms_full.png",
+"mapParseStartPos": [147, 139],
+"log": "log.txt",
+"port": 8080,
+"time": 0.1,
+"initialConnectTime": 1,
+"maxDataSize": 1024,
+"defaultClient": "../test-clients/python/",
+"log": "serverlog.json"
+},
