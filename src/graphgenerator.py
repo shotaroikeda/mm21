b5f771808f875a3be2e96170be3f24dbce2bf17e
Sam Laane <silverdev@ymail.com>
2015-03-07 15:12:50 -0600

    fixed formating

--- src/graphgenerator.py
+++ src/graphgenerator.py
@@ -1,35 +1,34 @@
 import uuid
 
-class Node(object):
-	def __init__(self,nodeType):
-		self.uid = uuid.uuid4()
-		self.nodeType = nodeType
-		self.connectedNodes = []
 
-	def join_node(self,node):
-		self.connectedNodes.append(node.nodeUid)
-		node.connectedNodes.append(self.nodeUid)
+class Node(object):
+        def __init__(self, nodeType):
+                self.uid = uuid.uuid4()
+                self.nodeType = nodeType
+                self.connectedNodes = []
+
+        def join_node(self, node):
+                self.connectedNodes.append(node.nodeUid)
+                node.connectedNodes.append(self.nodeUid)
+
 
 def generate_graph(numPlayers, cityDensity, ispDensity, connectionDensity):
-	clusterList = [list() for _ in range(numPlayers)]
-	for cluster in clusterList: 
-		for _ in range(ispDensity):
-			isp = Node("isp")
-			cluster.append(isp)
-			ispList = []
-			for city in range(cityDensity):
-				cityList = []
-				city = Node('city')
-				isp.join_node(city)
-				for c in cityList():
-					city.join_node(c)
-				cityList.append(city)
-	#ToDo: links betwean cities, datacenter
-	return clusterList
+        clusterList = [list() for _ in range(numPlayers)]
+        for cluster in clusterList:
+                for _ in range(ispDensity):
+                        isp = Node("isp")
+                        cluster.append(isp)
+                        ispList = []
+                        for city in range(cityDensity):
+                                cityList = []
+                                city = Node('city')
+                                isp.join_node(city)
+                                for c in cityList():
+                                        city.join_node(c)
+                                cityList.append(city)
+        # ToDo: links betwean cities, datacenter
+        return clusterList
 
-	
+
 
 if __name__ == "__main__":
-	
-
-	
