6956185bd0c2b00fc5af6ced7c56ab5c3c59cad2
Ace Nassri <ace.nassri@gmail.com>
2015-04-11 15:58:38 -0500

    put this back

---
+++ src/objects/node.py
@@ -0,0 +1,44 @@
+"""
+Holds data about a specific node on the map
+"""
+
+import game_constants
+class Node(object):
+    def __init__(self, id, adjacent, nodetype):
+        # int
+        self.id = id
+        self.processing = game_constants.processing[nodeType]
+        self.networking = game_constants.networking[nodeType]
+	self.remainingProcessing = game_constants.networking[nodeType]
+	self.remainingNetworking = game_constants.processing[nodeType]
+        self.ownerId = None
+        self.softwareLevel = 0
+        # int[]
+        self.adjacentIds = adjacent
+        self.rootkitIds = []
+        # bool
+        self.isDDOSed = False
+        self.isIPSed = False
+        # dict<int, int>
+        self.infiltration = dict()
+
+    def connect(self, other):
+        # other is a mapNode
+        if other.id in self.adjacentIds:
+            raise Exception("Nodes are already connected.")
+        self.adjacentIds.append(other.id)
+        other.adjacentIds.append(self.id)
+        return
+
+    def canMoveThrough(self, playerId):
+        if playerId in self.rootkitIds:
+            return True
+        return self.ownerId == playerId
+
+    def own(self, playerId):
+        if playerId == self.ownerId:
+            raise Exception("This player owns this node already.")
+        self.ownerId = playerId
+        self.rootkitIds = []
+        self.infiltration = []
+        return
