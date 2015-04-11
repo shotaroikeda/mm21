5d0f0eccddff84956ae45b823a017950b820df47
lalicari <lalicari94@gmail.com>
2015-04-11 16:10:33 -0500

    added instructions for test map

--- src_test/test_map.py
+++ src_test/test_map.py
@@ -1,6 +1,10 @@
 import src.objects.map as emap
 import pytest
 
+#to run, must have pytest installed see: http://pytest.org/latest/getting-started.html
+#to get the import statement to run correctly, you must run 
+#<export PYTHONPATH=`pwd`> in the root directory
+
 def test_addTeam():
 	_map = emap.Map()
 	_map.addTeam(1)
