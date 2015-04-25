d489ec7e0cd18f9da451c559178e4134e9457f99
Eric Sauer <ericmsauer@gmail.com>
2015-04-25 16:55:04 -0500

    Fixed graph generation (connecting conts)

--- src/map-generation/generate.py
+++ src/map-generation/generate.py
@@ -2,7 +2,7 @@ from map import Map
 import pprint
 
 if __name__ == "__main__":
-    m = Map(2, 4, 3)
+    m = Map(2, 2, 2)
     pp = pprint.PrettyPrinter()
     pp.pprint(m.convert_to_json())
-    m.draw_graph()
+    #m.draw_graph()
