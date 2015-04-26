9de254971bb6b707a4dae584cb8c1b5005bce3f0
Ace Nassri <ace.nassri@gmail.com>
2015-04-25 21:07:32 -0500

    Yay colors\!

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Colors (the ones we use)
+CGRN="\033[92m"
+CRED="\033[91m"
+CEND="\033[0m"
+FBOLD="\033[1m"
+
+# Initial message
 message(){
     echo ""
-    echo "Above is a list of errors detected in your code. Please fix them before committing!"
+    echo "$CREDAbove is a list of errors detected in your code. $FBOLD Please fix them before committing!$CEND"
     echo "You can bypass this check (and incur Ace's wrath) with \'git commit --no-verify\'"
 }
 
@@ -11,9 +18,9 @@ trap message 0
 # Find working directory + add it to $PYTHONPATH
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )" # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )" # Resolve $SOURCE until its no longer a symlink
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE is relative, resolve it relative to the symlink file's location
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # If $SOURCE is relative, resolve it relative to the symlink
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 echo $DIR
@@ -36,31 +43,31 @@ if hash pep8 2>/dev/null; then
         pep8 src_test --ignore=E122,E241,W293,W291,W391,E501,E126
     fi
 else
-    echo "Error: pep8 not found."
+    echo "$CRED $FBOLD Error: pep8 not found. $CEND"
     exit 1
 fi
 if hash py.test 2>/dev/null; then
         py.test
     else
-        echo "Error: pytest not found."
+        echo "$CGRN $FBOLD Error: pytest not found. $CEND"
         exit 1
 fi
-echo "All tests ran!"
+echo "$CGRN All tests ran! $CEND"
 
 # Install pre-commit hook
 FILE="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 HOOKPATH=$DIR/".git/hooks/pre-commit"
 if [ ! -L $HOOKPATH ]; then
-    echo "installing precommit hook"
+    echo "Installing pre-commit hook..."
     # Remove old hook file
     if  [ -f $HOOKPATH ]; then
         rm $HOOKPATH
     fi
     # Symlink to new hook file
     ln -s $DIR/$FILE $HOOKPATH
-    echo "pre-commit hoook installed"
+    echo "Pre-commit hoook installed!"
 fi
 
 # Done!
 trap - 0
-echo "Done!"
+echo "$CGRN $FBOLD Done! $CEND"
