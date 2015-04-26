f185f65183e129a33eecb2fe00045c7e5e684860
Ace Nassri <ace.nassri@gmail.com>
2015-04-25 22:15:57 -0500

    Fix pep8 errors

---
+++ src/misc_constants.py
@@ -0,0 +1,15 @@
+"""
+Holds constants used by the server and visualizer
+"""
+
+# Default server port
+port = 1337
+
+# Default map
+mapFile = "/dev/null"
+
+# Default logfile path
+logFile = "/dev/null"
+
+# Default client path
+defaultClient = "/dev/null"
