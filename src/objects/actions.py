e6775c76871d9eaa9f4621a2cc96373e9e148271
ace-n <nassri2@illinois.edu>
2015-05-26 17:57:28 -0400

    Bugfixes + gave actions a node class of their own - because partial classes don't work in Python, and actions are fundamentally node-based

--- src/objects/actions.py
+++ src/objects/actions.py
@@ -3,11 +3,6 @@ Data-mutators for actions players can do (to the map, other players, and/or node
 """
 
 
-# Raise an exception if resources are insufficient
-def requireResources(processingCost, networkingCost):
-    map.decrementPower(self, self.processingCost, self.networkingCost)
-
-
 class AttemptToMultipleDDosException(Exception):
     pass
 
@@ -16,47 +11,47 @@ class AttemptToMultipleRootkitException(Exception):
     pass
 
 
-def doControl(self, playerId, multiplier):
-    requireResources(multiplier, multiplier)
-    if playerId == self.ownerId:
-        for k in self.infiltration.iterkeys():
-            self.infiltration[k] = max(self.infiltration[k] - multiplier, 0)
-    else:
-        self.infiltration[playerId] = self.infiltration.get(playerId, 0) + multiplier
-        if self.infiltration[playerId] > 50:  # TODO change this number
-            self.own(playerId)
-    return
+# Node class used to model actions and their results
+class ActionNode:
 
+    # Raise an exception if resources are insufficient
+    def requireResources(self, processingCost, networkingCost):
+        map.decrementPower(self, self.processingCost, self.networkingCost)
 
-def doDDOS(self):
-    requireResources(self.totalPower / 5, self.totalPower / 5)
-    if self.DDoSStatus == DDoSStatus.PENDING:
-        raise AttemptToMultipleDDosException()
-    self.DDoSStatus = DDoSStatus.PENDING
+    def doControl(self, playerId, multiplier):
+        self.requireResources(multiplier, multiplier)
+        if playerId == self.ownerId:
+            for k in self.infiltration.iterkeys():
+                self.infiltration[k] = max(self.infiltration[k] - multiplier, 0)
+        else:
+            self.infiltration[playerId] = self.infiltration.get(playerId, 0) + multiplier
+            if self.infiltration[playerId] > 50:  # TODO change this number
+                self.own(playerId)
 
+    def doDDOS(self):
+        if self.DDoSStatus == DDoSStatus.PENDING:
+            raise AttemptToMultipleDDosException() 
+        self.requireResources(self.totalPower / 5, self.totalPower / 5)
+        self.DDoSStatus = DDoSStatus.PENDING
 
-def doUpgrade(self):
-    requireResources(self.processing, self.networking)
-    self.softwareLevel += 1
+    def doUpgrade(self):
+        self.requireResources(self.processing, self.networking)
+        self.softwareLevel += 1
 
+    def doClean(self):
+        self.requireResources(100, 0)
+        self.rootkitIds = []
 
-def doClean(self):
-    requireResources(100, 0)
-    self.rootkitIds = []
+    def doScan(self):
+        self.requireResources(25, 0)
+        return self.rootkitIds
 
+    def doRootkit(self, playerId):
+        if playerId in self.rootkitIds:
+            raise AttemptToMultipleRootkitException("This player has a rootkit here already.")
+        self.requireResources(self.totalPower / 5, self.totalPower / 5)
+        self.rootkitIds.append(playerId)
 
-def doScan(self):
-    requireResources(25, 0)
-    return self.rootkitIds
-
-
-def doRootkit(self, playerId):
-    requireResources(self.totalPower / 5, self.totalPower / 5)
-    if playerId in self.rootkitIds:
-        raise AttemptToMultipleRootkitException("This player has a rootkit here already.")
-    self.rootkitIds.append(playerId)
-
-
-def doPortScan(self):
-    requireResources(0, 500)
-    return self
+    def doPortScan(self):
+        self.requireResources(0, 500)
+        return self
