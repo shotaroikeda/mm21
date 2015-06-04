9dc1a9f7792550f2528bccb027a780f0f2de349f
Quansheng Tan <qtan4@illinois.edu>
2015-06-04 10:58:15 -0500

    pass all pep8 for vis

--- src/vis/vis_constants.py
+++ src/vis/vis_constants.py
@@ -14,9 +14,9 @@ datacenter_size = 10
 city_radius = 20
 city_size = 5
 
-#COLORS
-WHITE = (255,255,255)
-RED = (255, 0 , 0)
+# COLORS
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
--- src/vis/visualizer.py
+++ src/vis/visualizer.py
@@ -45,8 +45,7 @@ class Visualizer(object):
             x_blockSize = math.floor(self.screenWidth / blocks)
             y_blockSize = math.floor(self.screenHeight / blocks)
             center_x = (x_blockSize) * ((j % blocks) + 1 - 0.5) + random.randint(-math.floor(x_blockSize / 12), math.floor(x_blockSize / 12))
-            center_y = (y_blockSize) * (math.floor(j / blocks) + 1 - 0.5)+ random.randint(-math.floor(y_blockSize / 12), math.floor(y_blockSize / 12))
-
+            center_y = (y_blockSize) * (math.floor(j / blocks) + 1 - 0.5) + random.randint(-math.floor(y_blockSize / 12), math.floor(y_blockSize / 12))
 
             i = 0
             isp_amount = len(cont['isps'])
@@ -63,8 +62,8 @@ class Visualizer(object):
                 for city in isp['cities']:
                     self.draw_json[city.uid] = {}
                     self.draw_json[city.uid]['type'] = 'city'
-                    x_offset = random.randint(-math.floor(self.city_radius/ 4), math.floor(self.city_radius / 4))
-                    y_offset = random.randint(-math.floor(self.city_radius/ 4), math.floor(self.city_radius / 4))
+                    x_offset = random.randint(-math.floor(self.city_radius / 4), math.floor(self.city_radius / 4))
+                    y_offset = random.randint(-math.floor(self.city_radius / 4), math.floor(self.city_radius / 4))
                     self.draw_json[city.uid]['x'] = int(self.draw_json[isp['id']]['x'] + self.city_radius * math.cos((2 * math.pi / city_amount) * k)) + x_offset
                     self.draw_json[city.uid]['y'] = int(self.draw_json[isp['id']]['y'] + self.city_radius * math.sin((2 * math.pi / city_amount) * k)) + y_offset
                     k += 1
@@ -75,8 +74,8 @@ class Visualizer(object):
             for datacenter in cont['datacenters']:
                 self.draw_json[datacenter['id']] = {}
                 self.draw_json[datacenter['id']]['type'] = 'datacenter'
-                x_offset = random.randint(-math.floor(self.datacenter_radius/ 4), math.floor(self.datacenter_radius / 4))
-                y_offset = random.randint(-math.floor(self.datacenter_radius/ 4), math.floor(self.datacenter_radius / 4))
+                x_offset = random.randint(-math.floor(self.datacenter_radius / 4), math.floor(self.datacenter_radius / 4))
+                y_offset = random.randint(-math.floor(self.datacenter_radius / 4), math.floor(self.datacenter_radius / 4))
                 self.draw_json[datacenter['id']]['x'] = int(center_x + self.datacenter_radius * math.cos((2 * math.pi / datacenter_amount) * i)) + x_offset
                 self.draw_json[datacenter['id']]['y'] = int(center_y + self.datacenter_radius * math.sin((2 * math.pi / datacenter_amount) * i)) + y_offset
                 i += 1
@@ -109,7 +108,9 @@ class Visualizer(object):
                 pygame.draw.circle(self.screen, const.BLUE, [value['x'], value['y']], self.city_size)
         for edge in self.json_data['edges']:
             v1, v2 = edge
-            pygame.draw.line(self.screen, const.BLACK, [self.draw_json[v1]['x'], self.draw_json[v1]['y']],
-                                                     [self.draw_json[v2]['x'], self.draw_json[v2]['y']], 1)
+            pygame.draw.line(
+                    self.screen, const.BLACK, [self.draw_json[v1]['x'], self.draw_json[v1]['y']],
+                    [self.draw_json[v2]['x'], self.draw_json[v2]['y']], 1
+            )
         pygame.display.update()
         pygame.display.flip()
