c2977ea9736b64a8d607f77713ad42f8524dbd1b
Ace Nassri <ace.nassri@gmail.com>
2015-09-05 02:30:00 -0500

    stuff and things

--- vagrant/vagrant_init.sh
+++ vagrant/vagrant_init.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# Java
-add-apt-repository -y ppa:webupd8team/java
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+# MUST COME BEFORE ANY INSTALLS
 apt-get -y update
-apt-get -y install oracle-java8-installer
+
+# Ubuntu desktop
+apt-get -y install ubuntu-desktop
+
+# Java
+apt-get -y install openjdk-7-jre
 
 # Game dependencies
-apt-get -y update
 apt-get -y install pip
 apt-get -y install git
 pip install pygame
@@ -16,7 +18,6 @@ pip install pygame
 apt-get -y install vim
 apt-get -y install emacs
 apt-get -y install eclipse-platform
-apt-get -y install ubuntu-desktop
 
 # Useful dev tools
 pip install flake8
@@ -30,14 +31,6 @@ git remote add origin https://github.com/acm-uiuc/mm21
 # TODO Put on wiki
 EG="~/Desktop/eclipse_guide.txt"
 echo "" > $EG
-
-echo "How to use Eclipse" >> $EG
-echo "*** Check out the code (cd ~/Desktop/mm21 && git pull origin master) before starting this phase! ***" >> $EG
-echo "1) Click the 'dash home' button at the top left of your screen" >> $EG
-echo "2) Search for (and click on) Eclipse" >> $EG
-echo "3) File > New Project (name it 'mm21' or something similar)" >> $EG
-echo "4) Uncheck 'Use default location', and use '/root/Desktop/mm21/test-clients/java/' instead" >> $EG
-echo "5) Click 'Finish'" >> $EG
 
 # Go!
 startx
