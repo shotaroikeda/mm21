75f6b295805303b683383553517a5c469257bf1f
Ace Nassri <nassri2@illinois.edu>
2015-09-05 17:07:24 -0500

    Update vagrant_init.sh

--- vagrant/vagrant_init.sh
+++ vagrant/vagrant_init.sh
@@ -10,9 +10,9 @@ apt-get -y install ubuntu-desktop
 apt-get -y install openjdk-7-jre
 
 # Game dependencies
-apt-get -y install pip
 apt-get -y install git
-pip install pygame
+apt-get -y build-dep python-pygame
+apt-get -y install python-dev
 
 # Desktop + Editors/IDEs
 apt-get -y install vim
@@ -20,17 +20,13 @@ apt-get -y install emacs
 apt-get -y install eclipse-platform
 
 # Useful dev tools
+apt-get -y install python-pip
 pip install flake8
 
 # Game itself
 mkdir ~/Desktop/mm21 && cd ~/Desktop/mm21
 git init
 git remote add origin https://github.com/acm-uiuc/mm21
-
-# Eclipse instructions
-# TODO Put on wiki
-EG="~/Desktop/eclipse_guide.txt"
-echo "" > $EG
 
 # Go!
 startx
