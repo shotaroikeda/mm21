0ba0fe3462ec3646dd3fb9133eec1d86ad7f38be
shotaroikeda <ikeda.shot@gmail.com>
2015-10-04 13:25:05 -0500

    added scanning mechanism

--- test-clients/ubot1/test_client.py
+++ test-clients/ubot1/test_client.py
@@ -41,6 +41,7 @@ def processTurn(serverResponse):
     bestScore = 0
     target = None
     action = "control"
+    PHASE = []
 
     # Node lists
     attackedNodes = [x for x in myNodes if max(x["infiltration"]) != 0]
@@ -50,14 +51,84 @@ def processTurn(serverResponse):
 
     dangerNodes = [x for x in serverResponse["map"]
                    if x["owner"] != myId
-                   if max(x["infiltration"].drop(str(myId), 0)) != 0]
+                   if max({k: v for k, v in x["infiltration"].items()
+                           if k != str(myId)}) != 0]
 
     # Creation of dict to maintain the amount of things we own
+
+    # Construct specific otherNodes
+    other_small = []
+    other_medium = []
+    other_large = []
+    other_data = []
+    other_isp = []
+    other_not_rootkit = []
+
+    log("constructing nodes...")
+    for node in otherNodes:
+        if node["nodetype"] == "Small City":
+            other_small.append(node)
+        elif node["nodetype"] == "Medium City":
+            other_medium.append(node)
+        elif node["nodetype"] == "Large City":
+            other_large.append(node)
+        elif node["nodetype"] == "Data Center":
+            other_data.append(node)
+        elif node["nodetype"] == "ISP":
+            other_isp.append(node)
+        else:
+            log(node["nodetype"])
+
+        if len(node["rootkits"]) > 0:
+            log(node["rootkits"])
+        else:
+            other_not_rootkit.append(node)
+
+    # Determine PHASE
+    if len(attackedNodes) is not 0:
+        PHASE.append("DEFEND")
     
+    log(len(otherNodes))
+    for n in otherNodes:
+
+        if n["nodetype"] == "Small City":
+            log("found small node")
+
+            req = n["totalPower"] / 5
+
+            if myN < req or myP < req:
+                continue
+            else:
+                log("rootkiting node")
+                actions.append({
+                    "action": "rootkit",
+                    "target": n["id"]
+                })
+
     
     #log("Player {} P {} / N {}".format(myId, myP, myN))
     #log("MINE " + str([x["id"] for x in myNodes]))
     #log("VIS  " + str([x["id"] for x in otherNodes]))
+
+    ###################################################################################
+    # IDEA:                                                                           #
+    # Split this bot up into 3 "PHASES" where each "PHASE" has a different trigger    #
+    # Condition.                                                                      #
+    #                                                                                 #
+    # Phase 1 - Greedy, optimized                                                     #
+    #    In this phase we attempt to spread very quickly. We attempt to do this until #
+    #    Contact is established between a player                                      #
+    #                                                                                 #
+    #    1. Attempt to maximize taking nodes (tries to take the largest node it can)  #
+    #    2. Uses the remaining to try rootkiting more important adjacent nodes        #
+    #                                                                                 #
+    # Phase 2 - DDoS, Rootkit, upgrade chain                                          #
+    ###################################################################################
+
+    
+    
+    
+    
 
     # Send actions to the server
     return {
