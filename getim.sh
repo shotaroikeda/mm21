7f160993b4ef31c689a7977ee7c278fdacdd86b9
shotaroikeda <ikeda.shot@gmail.com>
2015-10-04 01:11:42 -0500

    added compatibility for ubot1

--- getim.sh
+++ getim.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 if [ $1 == "-dev" ]; then
     ./clean.sh
-    python src/gamerunner.py -c jacob -b -s
+    python src/gamerunner.py -c ubot1 -b -s
+elif [ $1 == "-self" ]; then
+    ./clean.sh
+    python src/gamerunner.py -t 5 -c ubot1 -c ubot1 -c ubot1 -c ubot1 -c ubot1 -b -s
 else
     ./clean.sh
     python src/gamerunner.py $*
